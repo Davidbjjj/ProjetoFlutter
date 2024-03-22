d9a1f930eb7e50d46326c94c9798fef3b3b7f830
David Pontes <pondavid103@gmail.com>
2024-03-22 15:28:21 -0300

    add:feat button

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:js_interop';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -34,40 +36,51 @@ class Contando extends State<Contador> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Clicou, Banhou'),
-        titleTextStyle: TextStyle(
-            fontSize: 50,
-            backgroundColor: const Color.fromARGB(255, 19, 80, 185),
-            color: Colors.white),
-      ),
-      body: Center(
-        child: GestureDetector(
-          onTap: () {
-            setState(() {
-              contado++;
+        appBar: AppBar(
+          title: Text('Clicou, Banhou'),
+          titleTextStyle: TextStyle(
+              fontSize: 50,
+              backgroundColor: const Color.fromARGB(255, 19, 80, 185),
+              color: Colors.white),
+        ),
+        body: Center(
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                contado++;
 
-              if (contado > 0) {
+                if (contado > 0) {
+                  banhar = 'banhou';
+                }
+              });
+            },
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Terminou $banhar',
+                  style: TextStyle(fontSize: 50),
+                ),
+                SizedBox(height: 20), // Espaço entre os textos
+                Text(
+                  'clique para banhar',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              if (banhar == '') {
                 banhar = 'banhou';
+              } else {
+                banhar = '';
               }
             });
           },
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Terminou $banhar',
-                style: TextStyle(fontSize: 50),
-              ),
-              SizedBox(height: 20), // Espaço entre os textos
-              Text(
-                'clique para banhar',
-                style: TextStyle(fontSize: 20),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+          child: const Icon(Icons.add),
+        ));
   }
 }
